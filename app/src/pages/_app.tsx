d03324dd282337f3f69ae0e71cfd5e8294d59a21
Yuki-TU <k.yuki.90219@icloud.com>
2022-06-15 23:50:09 +0900

    add:#5 apolloクライアントを使うための設定と全体に敵強するCSSの設定

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css';
+import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import { setServer } from '../../mocks';
+import apolloClient from '../libs/apollo';
+import '../styles/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
   // モックサーバーを起動
@@ -9,7 +11,11 @@ function App({ Component, pageProps }: AppProps) {
   }
 
   return (
+    <div className="mx-auto w-full md:w-[1230px]">
+      <ApolloProvider client={apolloClient}>
         <Component {...pageProps} />
+      </ApolloProvider>
+    </div>
   );
 }
 
