2ce48a2fba6c2d2e766caf4b362921e6d37f9ea2
Yuki-TU <k.yuki.90219@icloud.com>
2022-06-15 23:13:00 +0900

    add:#5 ホームコンポーネント追加

---
+++ app/src/components/templates/Home/presenter.tsx
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+import { GetAdultVideosQuery } from '../../../graphql/generated';
+import Footer from '../../Footer';
+import { Card } from '../../uiParts/Card';
+
+type Props = {
+  /** ビデオ一覧 */
+  adultVideos: GetAdultVideosQuery['AdultVideos'] | undefined;
+};
+
+/**
+ * ホームページのpresenterコンポーネント
+ *
+ * ディレクトリ: /
+ */
+const Home = (props: Props) => (
+  <>
+    <Head>
+      <title>nne - ホーム</title>
+      <meta name="description" content="nneのホームぺーじです。" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main>
+      <h1 className=" text-4xl font-bold  text-red-700">nne</h1>
+      <div className="flex flex-wrap gap-2 mb-[-20px] sm:mb-[-30px]">
+        {props.adultVideos?.map((video) => (
+          <div
+            className="mx-2 mb-[20px] w-full sm:mb-[30px] sm:w-[calc(100%/3.1)]"
+            key={video.name}
+          >
+            <Card
+              title={video.name}
+              description={video.name}
+              thumnnail={video.thumbnail.url}
+            />
+          </div>
+        ))}
+      </div>
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+  </>
+);
+export default Home;
