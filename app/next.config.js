7bba251e68f76d4e8f6a225668e6d77cf1f75727
Yuki-TU <k.yuki.90219@icloud.com>
2022-06-15 23:14:44 +0900

    add:#5 nextjs日本語の設定とnext/imageで使える外部のドメイン設定

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'ja'],
+    defaultLocale: 'ja',
+  },
+  images: {
+    domains: ['ei.phncdn.com', 'di.phncdn.com'],
+  },
 };
 
 module.exports = nextConfig;
