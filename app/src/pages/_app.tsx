4c3150ea750d77cdd707696ea476ae9cd0b32a53
Yuki-TU <k.yuki.90219@icloud.com>
2022-06-18 16:29:44 +0900

    update:#5 mocksはdev環境以外ではimportしないように修正 ファイルが重くなるため

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
-import { setServer } from '../../mocks';
 import apolloClient from '../libs/apollo';
 import '../styles/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
   // モックサーバーを起動
   if (process.env.NODE_ENV === 'development') {
+    const { setServer } = require('../../mocks');
     setServer();
   }
 
