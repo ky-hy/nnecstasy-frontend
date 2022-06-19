3d1b7725381c3f5ea28904d1be2d7de9ffbbf07f
Yuki-TU <k.yuki.90219@icloud.com>
2022-06-19 17:56:03 +0900

    add:#5 モックのデータを修正 * 4つにした * タイトルを長くした

--- app/mocks/api/video.ts
+++ app/mocks/api/video.ts
@@ -9,8 +9,8 @@ export const getAdultVideos = graphql.query(
       ctx.data({
         AdultVideos: [
           {
-            name: 'video_name0',
-            url: 'video_url0',
+            name: 'testtesttesttesttexztestestestesttestetesttesttestesttestesttestettestewstestswteststevideo_name0',
+            url: 'video_url0_t',
             thumbnail: {
               url: 'https://ei.phncdn.com//videos//202105//08//387713851//thumbs_1//(m=eaf8Ggaaaa)(mh=McPhC2ubGCFPKeuX)1.jpg',
             },
@@ -18,6 +18,20 @@ export const getAdultVideos = graphql.query(
           {
             name: 'video_name1',
             url: 'video_url1',
+            thumbnail: {
+              url: 'https://ei.phncdn.com//videos//202105//08//387713851//thumbs_1//(m=eaf8Ggaaaa)(mh=McPhC2ubGCFPKeuX)1.jpg',
+            },
+          },
+          {
+            name: 'video_name3',
+            url: 'video_url3',
+            thumbnail: {
+              url: 'https://ei.phncdn.com//videos//202105//08//387713851//thumbs_1//(m=eaf8Ggaaaa)(mh=McPhC2ubGCFPKeuX)1.jpg',
+            },
+          },
+          {
+            name: 'video_name4',
+            url: 'video_url4',
             thumbnail: {
               url: 'https://ei.phncdn.com//videos//202105//08//387713851//thumbs_1//(m=eaf8Ggaaaa)(mh=McPhC2ubGCFPKeuX)1.jpg',
             },
