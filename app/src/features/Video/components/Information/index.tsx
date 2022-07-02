9aa8ba010536cec06730f4df7dd1f4f25c8e65d3
Yuki-TU <k.yuki.90219@icloud.com>
2022-07-03 05:09:24 +0900

    add:#9 video視聴ページとその機能の作成

---
+++ app/src/features/Video/components/Information/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {
+  movieName: string | undefined;
+};
+
+/**
+ * 動画詳細情報
+ * @param param0.movieName 動画名
+ */
+export const Information: React.FC<Props> = ({ movieName }) => {
+  return (
+    <h2 className="mx-2 mt-4 text-xl font-bold break-words">{movieName}</h2>
+  );
+};
---
+++ app/src/features/Video/components/VideoPlayer/index.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable tailwindcss/no-custom-classname */
+import React from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+
+interface IVideoPlayerProps {
+  options: videojs.PlayerOptions;
+}
+
+const initialOptions: videojs.PlayerOptions = {
+  controls: true,
+  fluid: true,
+  controlBar: {
+    volumePanel: {
+      inline: false,
+    },
+  },
+};
+
+/**
+ * 動画を再生するためのプレイヤー
+ * @param param0 プレイヤーの設定
+ */
+const VideoPlayer: React.FC<IVideoPlayerProps> = ({ options }) => {
+  const videoNode = React.useRef<HTMLVideoElement>(null);
+  const player = React.useRef<videojs.Player>();
+
+  React.useEffect(() => {
+    player.current = videojs(videoNode.current!, {
+      ...initialOptions,
+      ...options,
+    }).ready(() => {});
+
+    return () => {
+      if (player.current) {
+        player.current.dispose();
+      }
+    };
+  }, [options]);
+
+  return <video ref={videoNode} className="video-js vjs-big-play-centered" />;
+};
+
+export default VideoPlayer;
