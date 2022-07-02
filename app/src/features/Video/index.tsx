a71be978f182f9fc54af4c26b308c1616c7c86e6
Yuki-TU <k.yuki.90219@icloud.com>
2022-07-03 05:09:24 +0900

    add:#9 video視聴ページとその機能の作成

---
+++ app/src/features/Video/index.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import 'video.js/dist/video-js.css';
+import { Information } from './components/Information';
+import VideoPlayer from './components/VideoPlayer';
+import { useQueryVideo } from './hooks/useQueryVideo';
+
+/**
+ * プレイヤーでビデオを表示する昨日
+ */
+const Video: React.FC = () => {
+  const { data, loading, error } = useQueryVideo();
+
+  if (loading) return <div>...loading</div>;
+  if (error) return <div>時間をおいてリロードしてください。</div>;
+
+  const videoJsOptions = {
+    sources: [{ src: data?.AdultVideo.url!, type: 'application/x-mpegURL' }],
+    poster: data?.AdultVideo.thumbnail.url!,
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{data?.AdultVideo.name} - nne</title>
+        <meta
+          name="description"
+          content={data?.AdultVideo.name + 'の動画です。'}
+        />
+      </Head>
+      <div className="m-0 mx-auto max-w-[800px]">
+        <VideoPlayer options={videoJsOptions} />
+        <Information movieName={data?.AdultVideo.name} />
+      </div>
+    </>
+  );
+};
+
+export default Video;
---
+++ app/src/pages/videos/[id].tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import 'video.js/dist/video-js.css';
+import Video from '../../features/Video';
+
+/**
+ * 動画表示ページ
+ */
+const VideoDetail: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>nne - nne</title>
+        <meta name="description" content="nneの公式サイトです。" />
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
+      <Video />
+    </>
+  );
+};
+
+export default VideoDetail;
