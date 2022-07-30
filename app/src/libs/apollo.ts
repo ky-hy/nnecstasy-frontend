d38131409d6f169346b9daa275b8d640dd122070
Yuki-TU <k.yuki.90219@icloud.com>
2022-07-31 06:07:44 +0900

    update:#13 test時にも使えるようにapollo clientを修正

--- app/src/libs/apollo.ts
+++ app/src/libs/apollo.ts
@@ -1,11 +1,23 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import 'cross-fetch/polyfill';
+
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+
+const link = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_NNE_API_URL, // エンドポイント設定
+  // Use explicit `window.fetch` so tha outgoing requests
+  // are captured and deferred until the Service Worker is ready.
+  // テストを実行する際以下の行内とうまく動作しない
+  fetch: (...args) => fetch(...args),
+});
+
+const cache = new InMemoryCache();
 
 /**
  * クライアントサイドでGraphQLを利用するための設定
  */
 const apolloClient = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_NNE_API_URL, // エンドポイント設定
-  cache: new InMemoryCache(), // キャッシュ設定
+  cache,
+  link,
 });
 
 export default apolloClient;
